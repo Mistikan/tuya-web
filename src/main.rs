48d017d602ded14a33e4382fa84c91558e091a44
Ермейкин Сергей <ermeikinsv@lad24.ru>
2024-12-03 21:09:33 +0300

    feat: add metrics endpoint

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use axum::{
     Router,
 };
 use clap::{CommandFactory, Parser};
+use log::warn;
+use metrics_exporter_prometheus::{PrometheusBuilder, PrometheusHandle};
 use outlet::Outlet;
 use std::iter::zip;
 use std::net::IpAddr;
@@ -16,6 +18,10 @@ mod outlet;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
+    /// Device name
+    #[arg(short, long)]
+    name: Vec<String>,
+
     /// Device ID
     #[arg(short, long)]
     device_ids: Vec<String>,
@@ -27,11 +33,16 @@ struct Args {
     /// IP address
     #[arg(short, long)]
     address: Vec<IpAddr>,
+
+    /// Version
+    #[arg(short, long)]
+    protocol_version: Vec<String>,
 }
 
 #[derive(Clone)]
 struct AppState {
     outlets: Arc<Vec<Outlet>>,
+    recorder_handle: Arc<PrometheusHandle>
 }
 
 #[tokio::main]
@@ -49,16 +60,21 @@ async fn main() {
             .exit();
     }
 
-    let outlets: Vec<_> = zip(zip(args.device_ids, args.key), args.address)
-        .map(|((dev_id, key), address)| Outlet {
+    let outlets: Vec<_> = zip(zip(zip(zip(args.device_ids, args.key), args.address), args.protocol_version), args.name)
+        .map(|((((dev_id, key), address), protocol_version), name)| Outlet {
+            name,
             dev_id,
             key,
             address,
+            protocol_version
         })
         .collect();
 
+    let recorder_handle = setup_metrics_recorder();
+
     let state = AppState {
         outlets: Arc::new(outlets),
+        recorder_handle: Arc::new(recorder_handle),
     };
 
     let app = Router::new()
@@ -67,10 +83,57 @@ async fn main() {
         .route("/outlet/:outlet_id", post(toggle_outlet))
         .route("/outlet/:outlet_id/:outlet_state", put(set_outlet))
         .route("/outlet/:outlet_id", get(get_outlet))
+        .route("/metrics", get(get_metrics))
         .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn get_metrics(
+    State(app_state): State<AppState>
+) -> (StatusCode, String) {
+    // Set flag tuya_smartplug_last_scrape_error
+    let mut tuya_smartplug_last_scrape_error = false; 
+
+    // Set metric tuya_smartplug_count_devices
+    let tuya_smartplug_count_devices = app_state.outlets.len();
+    metrics::gauge!("tuya_smartplug_count_devices").set(tuya_smartplug_count_devices as u8);
+
+    // Set device metrics
+    for outlet in app_state.outlets.iter() {
+        let device = outlet.name.clone();
+        match outlet.metrics().await {
+            Ok(dps) => {
+                metrics::gauge!("tuya_smartplug_current", "device" => device.clone()).set(f64::from(dps.current)/1000.0);
+                metrics::gauge!("tuya_smartplug_power", "device" => device.clone()).set(f64::from(dps.power)/100.0);
+                metrics::gauge!("tuya_smartplug_voltage", "device" => device.clone()).set(f64::from(dps.voltage)/100.0);
+                metrics::gauge!("tuya_smartplug_frequency", "device" => device.clone()).set(f64::from(dps.frequency)/100.0);
+
+            },
+            Err(e) => {
+                tuya_smartplug_last_scrape_error = true;
+                warn!("{}", e);
+            },
+        };
+    }
+
+    // Set global metrics
+    metrics::gauge!("tuya_smartplug_last_scrape_error").set(tuya_smartplug_last_scrape_error as i8);
+    metrics::counter!("tuya_smartplug_scrapes_total").increment(1);
+
+    // Return metrics
+    let metrics = app_state.recorder_handle.render();
+    return (StatusCode::OK, metrics);
+}
+
+fn setup_metrics_recorder() -> PrometheusHandle {
+    // https://github.com/tokio-rs/axum/blob/main/examples/prometheus-metrics/src/main.rs
+    // https://ellie.wtf/notes/exporting-prometheus-metrics-with-axum
+    // Ищи metrics::counter! и дергай эндпоинт, чтобы метрика появилась
+    PrometheusBuilder::new()
+        .install_recorder()
+        .unwrap()
 }
 
 // basic handler that responds with a static string
--- src/outlet.rs
+++ src/outlet.rs
@@ -5,11 +5,50 @@ use rust_async_tuyapi::{
     error::ErrorKind, mesparse::Message, tuyadevice::TuyaDevice, Payload, PayloadStruct,
 };
 use serde_json::json;
+use serde::{Deserialize, Serialize};
 
 pub struct Outlet {
+    pub name: String,
     pub dev_id: String,
     pub key: String,
     pub address: IpAddr,
+    pub protocol_version: String
+}
+
+#[derive(Serialize, Deserialize)]
+#[derive(Debug)]
+pub struct Dps {
+    #[serde(alias = "1")]
+    pub enable: Option<bool>,
+    #[serde(alias = "17")]
+    // TODO
+    pub xxx_todo: u16,
+    #[serde(alias = "18")]
+    pub current: u16,
+    #[serde(alias = "19")]
+    pub power: u16,
+    #[serde(alias = "20")]
+    pub voltage: u16,
+    #[serde(alias = "102")]
+    pub total_ele: u16,
+    #[serde(alias = "104")]
+    pub ovp: u16,
+    #[serde(alias = "105")]
+    pub ocp: u16,
+    #[serde(alias = "106")]
+    pub opp: u16,
+    #[serde(alias = "107")]
+    pub device_language: String,
+    #[serde(alias = "108")]
+    pub display_brightness: u16,
+    #[serde(alias = "109")]
+    pub standby_brightness: u16,
+    #[serde(alias = "110")]
+    pub enter_standby_time: u16,
+    #[serde(alias = "123")]
+    pub bill: u16,
+    #[serde(alias = "133")]
+    pub frequency: u16,
 }
 
 impl Outlet {
@@ -38,23 +77,55 @@ impl Outlet {
         debug!("payload sent");
 
         match receiver.recv().await {
+            Some(Ok(msgs)) => {
+                match Self::parse_state_messages(&msgs){
+                    Some(dps) => Ok(dps.enable.unwrap()),
+                    None => Err(ErrorKind::ParsingIncomplete),
+                }
+            },
+            Some(Err(e)) => Err(e),
+            None => Err(ErrorKind::TcpStreamClosed),
+        }
+    }
+
+    pub async fn metrics(&self) -> Result<Dps, ErrorKind> {
+        let mut device = self.device()?;
+        let current_time = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs() as u32;
+
+        let dps = json!({"18": {"command": {"dpId": [18, 19, 20]}}});
+
+        let payload = Payload::Struct(PayloadStruct {
+            dev_id: self.dev_id.to_string(),
+            gw_id: None,
+            uid: None,
+            t: Some(current_time.to_string()),
+            dp_id: None,
+            dps: Some(dps),
+        });
+
+        debug!("connecting...");
+        let mut receiver = device.connect().await?;
+        info!("connected.");
+        device.get(payload).await?;
+        debug!("payload sent");
+
+        match receiver.recv().await {
             Some(Ok(msgs)) => Self::parse_state_messages(&msgs).ok_or(ErrorKind::ParsingIncomplete),
             Some(Err(e)) => Err(e),
             None => Err(ErrorKind::TcpStreamClosed),
         }
     }
 
-    fn parse_state_message(msg: &Message) -> Option<bool> {
+    fn parse_state_message(msg: &Message) -> Option<Dps> {
         match &msg.payload {
             Payload::String(s) => {
                 let data: HashMap<String, serde_json::Value> = serde_json::from_str(s).ok()?;
                 let dps = data.get("dps")?;
-                let dps: HashMap<String, serde_json::Value> =
-                    serde_json::from_value(dps.clone()).ok()?;
-                match dps.get("1") {
-                    Some(serde_json::Value::Bool(b)) => Some(*b),
-                    _ => None,
-                }
+                let dps: Dps = serde_json::from_value(dps.clone()).ok()?;
+                Some(dps)
             }
             _ => {
                 debug!("no payload string in message, ignoring");
@@ -63,7 +134,7 @@ impl Outlet {
         }
     }
 
-    fn parse_state_messages(msgs: &[Message]) -> Option<bool> {
+    fn parse_state_messages(msgs: &[Message]) -> Option<Dps> {
         debug!("parsing received messages, there are {} msgs", msgs.len());
         if msgs.is_empty() {
             debug!("no message in stream");
@@ -111,6 +182,6 @@ impl Outlet {
     }
 
     fn device(&self) -> Result<TuyaDevice, ErrorKind> {
-        TuyaDevice::new("3.3", &self.dev_id, Some(&self.key), self.address)
+        TuyaDevice::new(&self.protocol_version, &self.dev_id, Some(&self.key), self.address)
     }
 }
